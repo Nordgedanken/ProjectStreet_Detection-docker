cfc288a454f3e30abbb6aa7f38e554999af2cdeb
Marcel <MTRNord@users.noreply.github.com>
2016-10-05 22:14:25 +0200

    Update generateHaarDetector_street.sh

--- scripts/generateHaarDetector_street.sh
+++ scripts/generateHaarDetector_street.sh
@@ -5,4 +5,5 @@ mkdir info
 
 python face_detect.py
 opencv_createsamples -img sample_street.jpg -bg bg.txt -info info/info.lst -pngoutput info -maxxangle 0.5 -maxyangle 0.5 -maxzangle 0.5 -num 1442
-opencv_createsamples -info info/info.lst -num 1442 -w 20 -h 20 -vec positives.vec
+opencv_createsamples -info info/info.lst -num 1400 -w 20 -h 20 -vec positives.vec
+opencv_traincascade -data data -vec positives.vec -bg bg.txt -numPos 1300 -numNeg 650 -numStages 10 -w 20 -h 20
