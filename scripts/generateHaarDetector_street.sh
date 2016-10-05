c681e40038b1226d98301bde2e0d8c8aba34da28
Marcel <MTRNord@users.noreply.github.com>
2016-10-05 22:04:23 +0200

    Update generateHaarDetector_street.sh

--- scripts/generateHaarDetector_street.sh
+++ scripts/generateHaarDetector_street.sh
@@ -3,4 +3,4 @@
 mkdir data
 mkdir info
 
-opencv_createsamples -img watch5050.jpg -bg bg.txt -info info/info.lst -pngoutput info -maxxangle 0.5 -maxyangle 0.5 -maxzangle 0.5 -num 1950
+opencv_createsamples -img sample_street.jpg -bg bg.txt -info info/info.lst -pngoutput info -maxxangle 0.5 -maxyangle 0.5 -maxzangle 0.5 -num 1950
