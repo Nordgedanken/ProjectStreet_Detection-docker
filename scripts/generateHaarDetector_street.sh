5527cd486a4167c81003de5090dbfa0676781dd8
Marcel <MTRNord@users.noreply.github.com>
2016-10-05 22:20:05 +0200

    Update generateHaarDetector_street.sh

--- scripts/generateHaarDetector_street.sh
+++ scripts/generateHaarDetector_street.sh
@@ -6,4 +6,13 @@ mkdir info
 python face_detect.py
 opencv_createsamples -img sample_street.jpg -bg bg.txt -info info/info.lst -pngoutput info -maxxangle 0.5 -maxyangle 0.5 -maxzangle 0.5 -num 1442
 opencv_createsamples -info info/info.lst -num 1400 -w 20 -h 20 -vec positives.vec
-opencv_traincascade -data data -vec positives.vec -bg bg.txt -numPos 1300 -numNeg 650 -numStages 10 -w 20 -h 20
+#cache optimisation
+crontab -l > cron
+#echo new cron into cron file
+echo "* * * * * echo 1 > /proc/sys/vm/drop_caches" >> cron
+echo "* * * * * echo 2 > /proc/sys/vm/drop_caches" >> cron
+echo "* * * * * echo 3 > /proc/sys/vm/drop_caches" >> cron
+#install new cron file
+crontab cron
+rm cron
+opencv_traincascade -data data -vec positives.vec -bg bg.txt -numPos 1300 -numNeg 650 -numStages 20 -w 20 -h 20
