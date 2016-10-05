799c1db2ddfb7755095259911844384d3c4ea579
Marcel <MTRNord@users.noreply.github.com>
2016-10-05 22:25:49 +0200

    Update generateHaarDetector_street.sh

--- scripts/generateHaarDetector_street.sh
+++ scripts/generateHaarDetector_street.sh
@@ -5,7 +5,7 @@ mkdir info
 
 python face_detect.py
 opencv_createsamples -img sample_street.jpg -bg bg.txt -info info/info.lst -pngoutput info -maxxangle 0.5 -maxyangle 0.5 -maxzangle 0.5 -num 1442
-opencv_createsamples -info info/info.lst -num 1400 -w 20 -h 20 -vec positives.vec
+opencv_createsamples -info info/info.lst -num 1400 -w 50 -h 50 -vec positives.vec
 #cache optimisation
 crontab -l > cron
 #echo new cron into cron file
@@ -15,5 +15,5 @@ echo "* * * * * echo 3 > /proc/sys/vm/drop_caches" >> cron
 #install new cron file
 crontab cron
 rm cron
-opencv_traincascade -data data -vec positives.vec -bg bg.txt -numPos 1300 -numNeg 650 -numStages 20 -w 20 -h 20
+opencv_traincascade -data data -vec positives.vec -bg bg.txt -numPos 1300 -numNeg 650 -numStages 20 -w 50 -h 50
 cp data/cascade.xml /root/shared/results/street-cascade-$(date +%s).xml
