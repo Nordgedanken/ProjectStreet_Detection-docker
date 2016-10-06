3f0348a9521a3fa5c3ec151ad0e5d2cb856714d6
Marcel <MTRNord@users.noreply.github.com>
2016-10-06 23:14:08 +0200

    Update download-image-by-link.py

--- scripts/download-image-by-link.py
+++ scripts/download-image-by-link.py
@@ -8,7 +8,7 @@ import os
 
 def store_raw_images():
     neg_images_link = '//image-net.org/api/text/imagenet.synset.geturls?wnid=n07942152'   
-    neg_image_urls = urllib2.request.urlopen(neg_images_link).read().decode()
+    neg_image_urls = urllib2.urlopen(neg_images_link).read().decode()
     pic_num = 1
     
     if not os.path.exists('neg'):
@@ -16,8 +16,8 @@ def store_raw_images():
         
     for i in neg_image_urls.split('\n'):
         try:
-            print(i)
-            urllib2.request.urlretrieve(i, "neg/"+str(pic_num)+".jpg")
+            i
+            urllib2.urlretrieve(i, "neg/"+str(pic_num)+".jpg")
             img = cv2.imread("neg/"+str(pic_num)+".jpg",cv2.IMREAD_GRAYSCALE)
             # should be larger than samples / pos pic (so we can place our image on it)
             resized_image = cv2.resize(img, (100, 100))
