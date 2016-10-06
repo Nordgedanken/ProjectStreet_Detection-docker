81619ce2363e2847dfb2dac56ef2aa8cecc9e940
Marcel <MTRNord@users.noreply.github.com>
2016-10-06 15:58:05 +0200

    Update generateHaarDetector_street.sh

--- scripts/generateHaarDetector_street.sh
+++ scripts/generateHaarDetector_street.sh
@@ -1,17 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
 mkdir data
 mkdir info
 
-PYTHONPATH="/usr/lib/python2.7/dist-packages/" python face_detect.py
+
+export PYTHONPATH="/usr/lib/python2.7/dist-packages/" 
+python face_detect.py
 opencv_createsamples -img sample_street.jpg -bg bg.txt -info info/info.lst -pngoutput info -maxxangle 0.5 -maxyangle 0.5 -maxzangle 0.5 -num 1442
 opencv_createsamples -info info/info.lst -num 1400 -w 50 -h 50 -vec positives.vec
 #cache optimisation
 crontab -l > cron
 #echo new cron into cron file
-echo "* * * * * echo 1 > /proc/sys/vm/drop_caches" >> cron
-echo "* * * * * echo 2 > /proc/sys/vm/drop_caches" >> cron
-echo "* * * * * echo 3 > /proc/sys/vm/drop_caches" >> cron
+{
+        echo "* * * * * echo 1 > /proc/sys/vm/drop_caches"
+        echo "* * * * * echo 2 > /proc/sys/vm/drop_caches"
+        echo "* * * * * echo 3 > /proc/sys/vm/drop_caches" 
+} >> cron
 #install new cron file
 crontab cron
 rm cron
