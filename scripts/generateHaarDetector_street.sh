f450ffc1c1ac3329a6a0c9e45f25ca94d4a05abe
Marcel <MTRNord@users.noreply.github.com>
2016-10-06 22:58:14 +0200

    Update generateHaarDetector_street.sh

--- scripts/generateHaarDetector_street.sh
+++ scripts/generateHaarDetector_street.sh
@@ -8,9 +8,9 @@ mkdir neg
 export PYTHONPATH="/usr/lib/python2.7/dist-packages/" 
 python download-image-by-link.py
 ls -la
-opencv_createsamples -img ./sample_street.jpg -bg bg.txt -info info/info.lst -pngoutput info -maxxangle 0.5 -maxyangle 0.5 -maxzangle 0.5 -num 1442
+opencv_createsamples -img ./sample_street.jpg -bg ./bg.txt -info ./info/info.lst -pngoutput info -maxxangle 0.5 -maxyangle 0.5 -maxzangle 0.5 -num 1442
 ls -la info
-opencv_createsamples -info info/info.lst -num 1400 -w 50 -h 50 -vec positives.vec
+opencv_createsamples -info ./info/info.lst -num 1400 -w 50 -h 50 -vec positives.vec
 ls -la neg
 ls -la
 #cache optimisation
@@ -24,5 +24,5 @@ crontab -l > cron
 #install new cron file
 crontab cron
 rm cron
-opencv_traincascade -data data -vec positives.vec -bg bg.txt -numPos 1300 -numNeg 650 -numStages 20 -w 50 -h 50
+opencv_traincascade -data ./data -vec ./positives.vec -bg ./bg.txt -numPos 1300 -numNeg 650 -numStages 20 -w 50 -h 50
 cp "data/cascade.xml" "/root/shared/results/street-cascade-$(date +%s).xml"
