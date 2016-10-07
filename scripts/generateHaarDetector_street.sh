e37466f91dbc6114b33478b1fc9d0922fbdd3241
Marcel <MTRNord@users.noreply.github.com>
2016-10-07 13:34:48 +0200

    Update generateHaarDetector_street.sh

--- scripts/generateHaarDetector_street.sh
+++ scripts/generateHaarDetector_street.sh
@@ -35,6 +35,7 @@ crontab cron
 rm cron
 
 echo "train haar cascade"
+NUM_FILES=$(ls -1 neg --file-type | grep -v '/$' | wc -l)-10
 NUM_FILES_HALF=$NUM_FILES/2
 opencv_traincascade -data ./data -vec ./positives.vec -bg ./bg.txt -numPos $NUM_FILES -numNeg $NUM_FILES_HALF -numStages 20 -w 50 -h 50
 cp "data/cascade.xml" "/root/shared/results/street-cascade-$(date +%s).xml"
