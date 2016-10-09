a4b1c4a3c8a175ced00d4fed02524c48903a86df
Marcel <MTRNord@users.noreply.github.com>
2016-10-09 15:39:59 +0200

    Update generateHaarDetector_street.sh

--- scripts/generateHaarDetector_street.sh
+++ scripts/generateHaarDetector_street.sh
@@ -1,25 +1,7 @@
 #!/bin/bash
 
-echo "Create Directories"
-mkdir data
-mkdir info
-mkdir neg
-
-
-echo "Download negatives"
 export PYTHONPATH="/usr/lib/python2.7/dist-packages/" 
-python download-image-by-link.py
-
-echo "generate poitives"
 NUM_FILES=$(ls -1 neg --file-type | grep -v '/$' | wc -l)-10
-ls -la
-opencv_createsamples -img ./sample_street.jpg -bg ./bg.txt -info ./info/info.lst -pngoutput info -maxxangle 0.5 -maxyangle 0.5 -maxzangle 0.5 -num $NUM_FILES
-ls -la info
-
-echo "generate vec files"
-opencv_createsamples -info ./info/info.lst -num $NUM_FILES -w 50 -h 50 -vec positives.vec
-ls -la neg
-ls -la
 
 echo "add cronjob"
 #cache optimisation
