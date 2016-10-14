ac15a86100236505d69c7a5eefb42ff1508aedee
Marcel <MTRNord@users.noreply.github.com>
2016-10-14 13:04:24 +0200

    Update stage1_3.sh

--- scripts/stage1_3.sh
+++ scripts/stage1_3.sh
@@ -3,3 +3,8 @@
 echo "Download negatives"
 export PYTHONPATH="/usr/lib/python2.7/dist-packages/" 
 python download-image-by-link.py --stage 1_3
+
+zstd --train pos/* -o pos
+zstd pos.zst -D pos
+cp "./pos.zst" "/root/shared/results/pos.zst" && echo "pos.zst in upload"
+cp "./bg.txt" "/root/shared/results/bg.txt" && echo "bg.txt in upload"
