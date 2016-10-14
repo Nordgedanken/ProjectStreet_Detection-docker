2dfeee34a96f0e2e67301c3521022669867aa0f8
Marcel <MTRNord@users.noreply.github.com>
2016-10-14 13:05:01 +0200

    Update stage1_1.sh

--- scripts/stage1_1.sh
+++ scripts/stage1_1.sh
@@ -9,3 +9,7 @@ mkdir neg
 echo "Download negatives"
 export PYTHONPATH="/usr/lib/python2.7/dist-packages/" 
 python download-image-by-link.py --stage 1_1
+
+zstd --train neg/* -o neg
+zstd neg.zst -D neg
+cp "./neg.zst" "/root/shared/results/neg.zst" && echo "pos.zst in upload"
