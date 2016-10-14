429d9d7a872c4aeeaabefe18d332e4daf82ac4ca
Marcel <MTRNord@users.noreply.github.com>
2016-10-14 13:02:16 +0200

    Create stage1_4.sh

---
+++ scripts/stage1_4.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "generate poitives"
+NUM_FILES=$(ls -1 neg --file-type | grep -v '/$' | wc -l)-10
+ls -la
+opencv_createsamples -img ./sample_street.jpg -bg ./bg.txt -info ./info/info.lst -pngoutput info -maxxangle 0.5 -maxyangle 0.5 -maxzangle 0.5 -num $NUM_FILES
+ls -la info
+
+echo "generate vec files"
+opencv_createsamples -info ./info/info.lst -num $NUM_FILES -w 50 -h 50 -vec positives.vec
+
+cp "./positives.vec" "/root/shared/results/positives.vec" && echo "positives.vec in upload"
