ef713fe53be71192ac4765dad40417749b811d1d
Marcel <MTRNord@users.noreply.github.com>
2016-10-14 14:56:50 +0200

    Update stage1_3.sh

--- scripts/stage1_3.sh
+++ scripts/stage1_3.sh
@@ -4,10 +4,12 @@ echo "Download negatives"
 export PYTHONPATH="/usr/lib/python2.7/dist-packages/" 
 python download-image-by-link.py --stage 1_3
 
-zstd --train pos/* -o pos
-zstd pos.zst -D pos
+zstd --train pos/* -o pos_dict
+zstd pos.zst -D pos_dict
+cp "./pos_dict" "/root/shared/results/pos_dict" && echo "pos_dict in upload"
 cp "./pos.zst" "/root/shared/results/pos.zst" && echo "pos.zst in upload"
 cp "./bg.txt" "/root/shared/results/bg.txt" && echo "bg.txt in upload"
-zstd --train neg/* -o neg
-zstd neg.zst -D neg
+zstd --train neg/* -o neg_dict
+zstd neg.zst -D neg_dict
+cp "./neg_dict" "/root/shared/results/neg_dict" && echo "neg_dict in upload"
 cp "./neg.zst" "/root/shared/results/neg.zst" && echo "pos.zst in upload"
